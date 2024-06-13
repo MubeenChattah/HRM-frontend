b3beba46ec428fa134282d75201bd2556a10eab0
MubeenChattah <19mubeen98@gmail.com>
2024-06-13 16:30:34 +0500

    test changing

--- src/Page/AddProduct.jsx
+++ src/Page/AddProduct.jsx
@@ -1,19 +1,15 @@
-import React from 'react'
-import SideBar from '../Component/SideBar'
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-
+import React from "react";
+import SideBar from "../Component/SideBar";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export default function AddProduct() {
   return (
-    <Box sx={{display:'flex'}}>
+    <Box sx={{ display: "flex" }}>
       <SideBar />
-      <Box component="main" sx={{ flexGrow: 1, p: 3 ,marginTop:"55px"}}>
-        
-      <Typography variant='h4'>
-            Add Product
-        </Typography>
+      <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: "55px" }}>
+        <Typography variant="h4">Add Products</Typography>
       </Box>
     </Box>
-  )
+  );
 }
